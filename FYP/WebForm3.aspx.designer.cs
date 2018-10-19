9bf6c812ecbe0e8b046c52adcb365c9070afe7b7
win10 <win10@DESKTOP-RG9B767>
2018-10-19 22:46:45 +0800

    fixed pos bug

--- FYP/WebForm3.aspx.designer.cs
+++ FYP/WebForm3.aspx.designer.cs
@@ -10,7 +10,34 @@
 namespace FYP {
     
     
-    public partial class WebForm2 {
+    public partial class WebForm3 {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// rowNo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField rowNo;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
         /// reportGridView control.
